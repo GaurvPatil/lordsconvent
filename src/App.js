b1a8b6900a75b5cd2996d113d43937fb302b9d6a
gaurav-patil <gaurvpatil17@gmail.com>
2023-03-12 21:57:32 +0530

    nabar stage- 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import Gallary from "./pages/Gallary";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-   <>
-   lord's convent setup DONE...
-   </>
-
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallary" element={<Gallary />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React from 'react'
+import Navbar from '../components/homeC/Nabar'
 
-
-const Home = () => {
-    return <>Home</>
+export const Home = () => {
+  return (
+   <>
+   <Navbar />
+   </>
+  )
 }
-
-export default Home
